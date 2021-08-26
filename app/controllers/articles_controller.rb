86b48933d445be670f72aafd103b2bc33fbe4daa
Pablo Centurión <pablushka@nomades.com.ar>
2021-08-26 08:57:31 -0300

    Added new functionality to the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
+  
   def index
     @name = "Biki"
   end
+
+  def authors
+    @authors_list = ["Borges", "Tolkien", "Biki", "Gonza"]
+  end
+
 end
